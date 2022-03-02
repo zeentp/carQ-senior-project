cb18715e441da227f38343e00a7324ad91fc67a3
n.wilailerdpradit <n.wilailerdpradit@accenture.com>
2022-03-02 19:53:46 +0700

    add fuction disablebtn

--- src/Booking.js
+++ src/Booking.js
@@ -9,19 +9,26 @@ import Select from "@mui/material/Select";
 import InputLabel from "@mui/material/InputLabel";
 import Stack from "@mui/material/Stack";
 import { Typography } from "@mui/material";
-
+import { useEffect, useState } from "react";
 export default function Booking() {
   const [fname, setFname] = React.useState('');
-  const [Lname, setLname] = React.useState('');
+  const [lname, setLname] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [phoneNumber, setPhoneNumber] = React.useState('');
   const [plateNumber, setPlateNumber] = React.useState('');
   const [carbrand, setCarBrand] = React.useState('');
   const [dateTime, setDateTime] = React.useState('');
   const [description, setDescription] = React.useState('');
+  const [disableApplyButton, setDisableApplyButton] = React.useState(false);
 
 
-
+  useEffect(() => {
+    if (fname !== '' && lname !== '' && email !=='' && phoneNumber.length === 12 && phoneNumber.length !== '' && plateNumber !== '' && carbrand !== null && dateTime !== null && description !== '') {
+      setDisableApplyButton(false)
+    } else {
+      setDisableApplyButton(true)
+    }
+  }, [fname, lname,email,phoneNumber,plateNumber,carbrand,dateTime,description]);
 
   const handlePhoneChange = (event) => {
     var val = event.target.value.replace(/[^0-9]/g, "");
@@ -39,19 +46,14 @@ export default function Booking() {
   const handleSubmit = (event) => {
     alert('hello')
     console.log(fname)
-    console.log(fname)
+    console.log(lname)
     console.log(email)
     console.log(phoneNumber)
     console.log(plateNumber)
     console.log(description)
     console.log(dateTime)
-
-
-
-
-
     event.preventDefault();
-    
+
   };
   return (
     <Box sx={{ flexGrow: 1, pl: 40, pr: 10 }}>
@@ -59,114 +61,119 @@ export default function Booking() {
         Make new appointment
       </Typography>
       <Box component="form" noValidate onSubmit={handleSubmit}>
-      <Grid sx={{ pt: 3 }} container spacing={3}>
-        <Grid item xs={6}>
-          <TextField
-            id="standard-multiline-flexible"
-            label="First Name"
-            fullWidth
-            maxRows={4}
+        <Grid sx={{ pt: 3 }} container spacing={3}>
+          <Grid item xs={6}>
+            <TextField
+              id="standard-multiline-flexible"
+              label="First Name"
+              fullWidth
+              maxRows={4}
+              // value={value}
+              onChange={(e) => setFname(e.target.value)}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              id="standard-multiline-flexible"
+              label="Last Name"
+              fullWidth
+              maxRows={4}
+              onChange={(e) => setLname(e.target.value)}
             // value={value}
-            onChange={(e) => setFname(e.target.value)}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <TextField
-            id="standard-multiline-flexible"
-            label="Last Name"
-            fullWidth
-            maxRows={4}
-            onChange={(e) => setLname(e.target.value)}
-          // value={value}
-          // onChange={handleChange}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <TextField
-            id="standard-multiline-flexible"
-            label="Email"
-            fullWidth
-            maxRows={4}
-            onChange={(e) => setEmail(e.target.value)}
-          // value={value}
-          // onChange={handleChange}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <TextField
-            id="standard-multiline-flexible"
-            label="TEL"
-            fullWidth
-            maxRows={4}
-            inputProps={{ maxLength: 12 }}
+            // onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              id="standard-multiline-flexible"
+              label="Email"
+              fullWidth
+              maxRows={4}
+              onChange={(e) => setEmail(e.target.value)}
             // value={value}
-            onChange={handlePhoneChange}
-            value={phoneNumber}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <TextField
-            id="standard-multiline-flexible"
-            label="Plate Number"
-            fullWidth
-            maxRows={4}
-            onChange={(e) => setPlateNumber(e.target.value)}
-          // value={value}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Car brand</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={carbrand}
-              label="car brand"
-              // onChange={handleChange}
-              onChange={(e) => setCarBrand(e.target.value)}
+            // onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              id="standard-multiline-flexible"
+              label="TEL"
+              fullWidth
+              maxRows={4}
+              inputProps={{ maxLength: 12 }}
+              // value={value}
+              onChange={handlePhoneChange}
+              value={phoneNumber}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              id="standard-multiline-flexible"
+              label="Plate Number"
+              fullWidth
+              maxRows={4}
+              onChange={(e) => setPlateNumber(e.target.value)}
+            // value={value}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Car brand</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={carbrand}
+                label="car brand"
+                // onChange={handleChange}
+                onChange={(e) => setCarBrand(e.target.value)}
 
-            >
-              <MenuItem value={"Honda"}>Honda</MenuItem>
-              <MenuItem value={"Yamaha"}>Yamaha</MenuItem>
-              <MenuItem value={"Toyoya"}>Toyoya</MenuItem>
-              <MenuItem value={"Toyoya"}>Nissan</MenuItem>
-            </Select>
-          </FormControl>
-        </Grid>
-        <Grid item xs={6}>
-          <TextField
-            id="datetime-local"
-            fullWidth
-            label="Date-Time"
-            type="datetime-local"
-            defaultValue="2022-05-24T10:30"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            onChange={(e) => setDateTime(e.target.value)}
+              >
+                <MenuItem value={"Honda"}>Honda</MenuItem>
+                <MenuItem value={"Yamaha"}>Yamaha</MenuItem>
+                <MenuItem value={"Toyoya"}>Toyoya</MenuItem>
+                <MenuItem value={"Toyoya"}>Nissan</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              id="datetime-local"
+              fullWidth
+              label="Date-Time"
+              type="datetime-local"
+              defaultValue="2022-05-24T10:30"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              onChange={(e) => setDateTime(e.target.value)}
 
-          />
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              id="standard-multiline-flexible"
+              label="Description"
+              fullWidth
+              maxRows={4}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+
+          </Grid>
+
+          <Grid item xs={12} container spacing={2} justifyContent="flex-end">
+            <Stack direction="row" spacing={2} sx={{ pt: 2 }}>
+              <Button>
+                Cancel
+              </Button>
+              <Button
+              
+                type="submit"
+                variant="contained"
+                disabled={disableApplyButton}
+              >Confirm</Button>
+            </Stack>
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <TextField
-            id="standard-multiline-flexible"
-            label="Description"
-            fullWidth
-            maxRows={4}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-          
-        </Grid>
-        
-        <Grid item xs={12} container spacing={2} justifyContent="flex-end">
-          <Stack direction="row" spacing={2} sx={{ pt: 2 }}>
-            <Button color="inherit" variant="contained">
-              Cancel
-            </Button>
-            <Button type="submit" variant="contained">Confirm</Button>
-          </Stack>
-        </Grid>
-      </Grid>
       </Box>
     </Box>
   );
